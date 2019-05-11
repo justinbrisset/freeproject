d522ccbe119b9b2662c2d4086e018472c872d0f7
Nina Akpovi <ninalaurence.akpovi@gmail.com>
2019-05-11 14:36:38 +0200

    git push

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router  = express.Router();
 // const User = require('../models/users');
 
-const bcrypt         = require("bcryptjs");
+const bcrypt         = require("bcrypt");
 const bcryptSalt     = 10;
 
 
